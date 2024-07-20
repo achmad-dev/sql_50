77faa414b587e0ecc870987ee8ecb1c2ec528d1f
achmad <faa04807@gmail.com>
2024-07-20 21:34:17 +0700

    update 620 solution

--- solutions/620.sql
+++ solutions/620.sql
@@ -48,5 +48,5 @@
     Explanation: 
     We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
 */
---- Solution: using not like
-SELECT id, movie, description, rating FROM Cinema WHERE id % 2 != 0 AND description NOT LIKE 'boring' ORDER BY rating DESC;
+--- Solution: check id and description
+SELECT id, movie, description, rating FROM Cinema WHERE id % 2 != 0 AND description != 'boring' ORDER BY rating DESC;
